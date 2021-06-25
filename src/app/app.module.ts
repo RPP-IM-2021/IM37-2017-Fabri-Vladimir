4f6dcf1d774f05f7ddc2f524f642ac1182c7b93c
VladimiFabri98 <vfficial98@gmail.com>
2021-06-25 21:29:44 +0200

    Inicijalni deo kreiran

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule} from '@angular/material/expansion';
+import { MatListModule} from '@angular/material/list';
+import { MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { ProizvodComponent } from './proizvod/proizvod.component';
+import { ProizvodjacComponent } from './proizvodjac/proizvodjac.component';
+import { RacunComponent } from './racun/racun.component';
+import { StavkaRacunaComponent } from './stavka-racuna/stavka-racuna.component';
+import { HomeComponent } from './core/home/home.component';
+import { AutorComponent } from './core/autor/autor.component';
+import { AboutComponent } from './core/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProizvodComponent,
+    ProizvodjacComponent,
+    RacunComponent,
+    StavkaRacunaComponent,
+    HomeComponent,
+    AutorComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
